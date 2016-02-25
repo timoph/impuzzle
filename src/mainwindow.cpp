bed245a34cacd33072141d51088f04a23ba85530
Timo Härkönen <timo.harkonen@kapsi.fi>
2016-02-25 09:50:00 +0200

    merge changes from github - lisence file - always showing window borders - gitignore - mac build script

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -52,10 +52,6 @@ MainWindow::MainWindow(QWidget *parent) :
 
     connect(GameView::instance(), SIGNAL(gameWon()), this, SLOT(gameEnded()));
     connect(GameView::instance(), SIGNAL(gameRestored()), this, SLOT(enableSaving()));
-
-    #ifndef Q_WS_MAEMO_5
-    setWindowFlags(Qt::FramelessWindowHint);
-    #endif
 }
 
 MainWindow *MainWindow::instance()
