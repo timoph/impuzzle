b27d532dbb9120f2571098ed3feb0a106f2d6d3f
Timo Rautiainen <212622+timoph@users.noreply.github.com>
2023-04-28 08:20:20 +0300

    add option to close application from the ui

--- languages/impuzzle2_fi.ts
+++ languages/impuzzle2_fi.ts
@@ -4,50 +4,55 @@
 <context>
     <name>GameMenu</name>
     <message>
-        <location filename="../qml/GameMenu.qml" line="30"/>
+        <location filename="../qml/GameMenu.qml" line="48"/>
         <source>New game</source>
         <translation>Uusi peli</translation>
     </message>
     <message>
-        <location filename="../qml/GameMenu.qml" line="45"/>
+        <location filename="../qml/GameMenu.qml" line="63"/>
         <source>Select image</source>
         <translation>Valitse kuva</translation>
     </message>
     <message>
-        <location filename="../qml/GameMenu.qml" line="54"/>
+        <location filename="../qml/GameMenu.qml" line="72"/>
         <source>image:</source>
         <translation>Kuva:</translation>
     </message>
     <message>
-        <location filename="../qml/GameMenu.qml" line="88"/>
+        <location filename="../qml/GameMenu.qml" line="107"/>
         <source>Show numbers</source>
         <translation>Näytä numerot</translation>
     </message>
     <message>
-        <location filename="../qml/GameMenu.qml" line="105"/>
+        <location filename="../qml/GameMenu.qml" line="124"/>
         <source>Make it harder!</source>
         <translation>Vaikeampi peli!</translation>
+    </message>
+    <message>
+        <location filename="../qml/GameMenu.qml" line="141"/>
+        <source>Quit</source>
+        <translation>Sulje ohjelma</translation>
     </message>
 </context>
 <context>
     <name>GameOverBanner</name>
     <message>
-        <location filename="../qml/GameOverBanner.qml" line="25"/>
+        <location filename="../qml/GameOverBanner.qml" line="43"/>
         <source>Puzzle solved</source>
         <translation>Peli ratkaistu</translation>
     </message>
     <message>
-        <location filename="../qml/GameOverBanner.qml" line="35"/>
+        <location filename="../qml/GameOverBanner.qml" line="53"/>
         <source>Completion required</source>
         <translation>Ratkaisu vaati</translation>
     </message>
     <message>
-        <location filename="../qml/GameOverBanner.qml" line="35"/>
+        <location filename="../qml/GameOverBanner.qml" line="53"/>
         <source>move</source>
         <translation>siirron</translation>
     </message>
     <message>
-        <location filename="../qml/GameOverBanner.qml" line="35"/>
+        <location filename="../qml/GameOverBanner.qml" line="53"/>
         <source>moves</source>
         <translation>siirtoa</translation>
     </message>
@@ -55,17 +60,17 @@
 <context>
     <name>ImagePicker</name>
     <message>
-        <location filename="../qml/ImagePicker.qml" line="16"/>
+        <location filename="../qml/ImagePicker.qml" line="34"/>
         <source>No images found</source>
         <translation>Yhtään kuvaa ei löytynyt</translation>
     </message>
     <message>
-        <location filename="../qml/ImagePicker.qml" line="29"/>
+        <location filename="../qml/ImagePicker.qml" line="47"/>
         <source>Choose image</source>
         <translation>Valitse kuva</translation>
     </message>
     <message>
-        <location filename="../qml/ImagePicker.qml" line="106"/>
+        <location filename="../qml/ImagePicker.qml" line="123"/>
         <source>preparing image...</source>
         <translation>Valmistellaan kuvaa...</translation>
     </message>
@@ -73,7 +78,7 @@
 <context>
     <name>LoadingScreen</name>
     <message>
-        <location filename="../qml/LoadingScreen.qml" line="45"/>
+        <location filename="../qml/LoadingScreen.qml" line="63"/>
         <source>Finding images...</source>
         <translation>Etsitään kuvia...</translation>
     </message>
@@ -81,42 +86,50 @@
 <context>
     <name>QCoreApplication</name>
     <message>
-        <location filename="../main.cpp" line="47"/>
+        <location filename="../main.cpp" line="67"/>
         <source>Define how many moves are made to shuffle the pieces.</source>
         <translation>Määrittää kuinka monta siirtoa palojen sekoittamiseen käytetään.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="48"/>
+        <location filename="../main.cpp" line="68"/>
         <source>count</source>
         <translation>lukumäärä</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="50"/>
         <source>Define path where to look for images</source>
-        <translation>Määrittään hakemiston, josta kuvia etsitään</translation>
+        <translation type="vanished">Määrittään hakemiston, josta kuvia etsitään</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="51"/>
+        <location filename="../main.cpp" line="71"/>
         <source>directory path</source>
         <translation>hakemistopolku</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="53"/>
         <source>Define image used to the puzzle</source>
-        <translation>Määrittää pelissä käytetyn kuvan</translation>
+        <translation type="vanished">Määrittää pelissä käytetyn kuvan</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="54"/>
+        <location filename="../main.cpp" line="70"/>
+        <source>Define path where to look for images.</source>
+        <translation>Polku josta kuvia etsitään</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="73"/>
+        <source>Define image used to create the puzzle.</source>
+        <translation>Pelissä käytettävä kuva</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="74"/>
         <source>file path</source>
         <translation>tiedostopolku</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
+        <location filename="../main.cpp" line="76"/>
         <source>Define language.</source>
         <translation>Valitse kieli.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
+        <location filename="../main.cpp" line="77"/>
         <source>en|fi|sv</source>
         <translation>en|fi|sv</translation>
     </message>
@@ -125,12 +138,17 @@
         <translation type="vanished">en|fi|sv</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
+        <location filename="../main.cpp" line="79"/>
         <source>Start in fullscreen.</source>
         <translation>Käynnistä sovellus koko ruudussa.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
+        <location filename="../main.cpp" line="81"/>
+        <source>Show more fireworks after completing the puzzle.</source>
+        <translation>Enemmän ilotulituksia</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="83"/>
         <source>Show help.</source>
         <translation>Näytä nämä selitteet.</translation>
     </message>
